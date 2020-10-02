c190186579102291a05eb734fa492ea69d9639c6
ricardont <rontiverosmarquez@gmail.com>
2020-10-01 23:19:09 -0600

    customer field

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,32 +1,20 @@
 class AppointmentsController < ApplicationController
   before_action :set_appointment, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_user!
-  # GET /appointments
-  # GET /appointments.json
   def index
     @appointments = Appointment.all
   end
   def calendar
   end
-  # GET /appointments/1
-  # GET /appointments/1.json
   def show
   end
-
-  # GET /appointments/new
   def new
     @appointment = Appointment.new
   end
-
-  # GET /appointments/1/edit
   def edit
   end
-
-  # POST /appointments
-  # POST /appointments.json
   def create
-    @appointment = Appointment.new(appointment_params)
-
+    @appointment = current_user.appointments.new(appointment_params)
     respond_to do |format|
       if @appointment.save
         format.html { redirect_to @appointment, notice: 'Cita creada.' }
@@ -37,9 +25,6 @@ class AppointmentsController < ApplicationController
       end
     end
   end
-
-  # PATCH/PUT /appointments/1
-  # PATCH/PUT /appointments/1.json
   def update
     respond_to do |format|
       if @appointment.update(appointment_params)
@@ -51,9 +36,6 @@ class AppointmentsController < ApplicationController
       end
     end
   end
-
-  # DELETE /appointments/1
-  # DELETE /appointments/1.json
   def destroy
     @appointment.destroy
     respond_to do |format|
@@ -61,14 +43,10 @@ class AppointmentsController < ApplicationController
       format.json { head :no_content }
     end
   end
-
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_appointment
       @appointment = Appointment.find(params[:id])
     end
-
-    # Only allow a list of trusted parameters through.
     def appointment_params
       params.require(:appointment).permit(:date_time, :duration, :user_id, :customer_id, :location_id, :product_id, :status)
     end
--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,31 +1,18 @@
 class CustomersController < ApplicationController
   before_action :set_customer, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_user!
-  # GET /customers
-  # GET /customers.json
   def index
     @customers = Customer.all
   end
-
-  # GET /customers/1
-  # GET /customers/1.json
   def show
   end
-
-  # GET /customers/new
   def new
-    @customer = Customer.new
+    @customer = current_user.customers.new
   end
-
-  # GET /customers/1/edit
   def edit
   end
-
-  # POST /customers
-  # POST /customers.json
   def create
-    @customer = Customer.new(customer_params)
-
+    @customer = current_user.customers.new(customer_params)
     respond_to do |format|
       if @customer.save
         format.html { redirect_to @customer, notice: 'Customer was successfully created.' }
@@ -36,9 +23,6 @@ class CustomersController < ApplicationController
       end
     end
   end
-
-  # PATCH/PUT /customers/1
-  # PATCH/PUT /customers/1.json
   def update
     respond_to do |format|
       if @customer.update(customer_params)
@@ -50,9 +34,6 @@ class CustomersController < ApplicationController
       end
     end
   end
-
-  # DELETE /customers/1
-  # DELETE /customers/1.json
   def destroy
     @customer.destroy
     respond_to do |format|
@@ -60,14 +41,10 @@ class CustomersController < ApplicationController
       format.json { head :no_content }
     end
   end
-
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_customer
       @customer = Customer.find(params[:id])
     end
-
-    # Only allow a list of trusted parameters through.
     def customer_params
       params.require(:customer).permit(:name, :phone, :email, :user_id)
     end
